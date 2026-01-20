65e1cbbc83093dfa8d4d5d2099063f678ff49137
chuanwei <463266963@qq.com>
2026-01-20 16:12:40 +0800

    Site updated: 2026-01-20 16:12:39

--- love/functions.js
+++ love/functions.js
@@ -38,9 +38,26 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
+	var current = new Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+	var totalDays = Math.floor(seconds / (3600 * 24));
+	// 计算年、月、日
+    var years = current.getFullYear() - date.getFullYear();
+    var months = current.getMonth() - date.getMonth();
+    var days = current.getDate() - date.getDate();
+    // 调整月份和天数
+    if (days < 0) {
+        months--;
+        // 获取上个月的天数
+        var lastDay = new Date(current.getFullYear(), current.getMonth(), 0).getDate();
+        days += lastDay;
+    }
+    
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+	//计算时分秒
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
@@ -55,6 +72,44 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+    var result = "<span class=\"digit\">" + totalDays + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒 【<span class=\"digit\">" + years + "</span>年<span class=\"digit\">" + months + "</span>个月<span class=\"digit\">" + days + "</span>天】"; 
 	$("#clock").html(result);
+}
+
+function timeElapseTogether(date){
+	var current = new Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var totalDays = Math.floor(seconds / (3600 * 24));
+	// 计算年、月、日
+    var years = current.getFullYear() - date.getFullYear();
+    var months = current.getMonth() - date.getMonth();
+    var days = current.getDate() - date.getDate();
+    // 调整月份和天数
+    if (days < 0) {
+        months--;
+        // 获取上个月的天数
+        var lastDay = new Date(current.getFullYear(), current.getMonth(), 0).getDate();
+        days += lastDay;
+    }
+    
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+    var result = "<span class=\"digit\">" + totalDays + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒 【<span class=\"digit\">" + years + "</span>年<span class=\"digit\">" + months + "</span>个月<span class=\"digit\">" + days + "</span>天】"; 
+    $("#clock-together").html(result);
 }
